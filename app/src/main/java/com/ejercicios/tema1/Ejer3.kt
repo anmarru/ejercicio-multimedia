2b04fd1db0d4bdac4d7a5398c806f0697be541ba
andrea <andreamartinezruiz10@gmail.com>
2024-09-20 14:00:04 +0200

    ejercicio 3 hecho

--- app/src/main/java/com/ejercicios/tema1/Ejer3.kt
+++ app/src/main/java/com/ejercicios/tema1/Ejer3.kt
@@ -1,5 +1,29 @@
+/*Ejercicio 3. Define las siguientes expresiones lambda en variables:
+a) Calcula el cubo de un número.
+b) Transforma una cadena de mayúscula a minúscula e invócala pasando
+como parámetro una lista de palabras usando la función map que recibe
+un lambda como parámetro.*/
 package com.ejercicios.tema1
 
+    fun cubo(a: Int): Int{
+    return a*a*a
+    }
+
+
+
+
  fun main() {
-    print(7)
-}
+    println(cubo(3))
+
+
+
+         // Lista de palabras en mayúsculas
+         val palabras = listOf("HOLA", "MUNDO", "KOTLIN")
+
+         // Usando map con una lambda para transformar a minúsculas
+         val palabrasMinusculas = palabras.map { it.lowercase() }
+
+         // Imprime la lista transformada
+         println(palabrasMinusculas)
+
+ }
