b7103eafd6cc33d848cf6e9515dbb635aada401c
andrea <andreamartinezruiz10@gmail.com>
2024-09-23 09:26:33 +0200

    ejercicio 4 hecho

--- app/src/main/java/com/ejercicios/tema1/Ejer4.kt
+++ app/src/main/java/com/ejercicios/tema1/Ejer4.kt
@@ -1,5 +1,18 @@
+/*Define una función llamada funcionMatematica, que dada un
+parámetro Int y un parámetro lambda, devuelva ese número tras aplicarle esa
+operación lambda.*/
 package com.ejercicios.tema1
 
 class Ejer4 {
-
 }
+    fun funcionMatematica(numero: Int, operacion: (Int) -> Int): Int {
+        return operacion(numero)
+    }
+
+    fun main() {
+        // Ejemplo de uso con una lambda que duplica el número
+        val resultado = funcionMatematica(5) { it * 2 }
+        println("El resultado es: $resultado") // Imprime: El resultado es: 10
+    }
+
+
